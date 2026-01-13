9878ae492a664fd7ee142f6210bb74f421c4afc4
faith-notes <124418034+tannnnnnsdf@users.noreply.github.com>
2026-01-13 14:05:18 +0700

    Create header.js

---
+++ js/header.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    const href = this.getAttribute('href');
+    if (href !== '#') {
+      e.preventDefault();
+      const target = document.querySelector(href);
+      if (target) {
+        target.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      }
+    }
+  });
+});
